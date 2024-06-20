df530186fbd85b5bce17c13061337c9eb46e26d5
Gerardo Garcia <gerardojgarcia@pm.me>
2024-06-20 14:03:43 -0700

    lesson 2.3

--- start/routes.ts
+++ start/routes.ts
@@ -7,12 +7,17 @@
 |
 */
 
+import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
+
+import fs from 'node:fs/promises'
 
 router.on('/').render('pages/home').as('home')
 
 router
   .get('/movies/:slug', async (ctx) => {
-    return ctx.view.render('pages/movies', { movie: ctx.params.slug })
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    const movie = await fs.readFile(url, 'utf8')
+    return ctx.view.render('pages/movies', { movie })
   })
   .as('movies.show')
