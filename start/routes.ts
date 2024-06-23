fc840a60d22ee0e101fb771c0c86d7741ec2ab47
Gerardo Garcia <gerardojgarcia@pm.me>
2024-06-23 09:50:30 -0700

    updates

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,7 @@
 |
 */
 
+import { Exception } from '@adonisjs/core/exceptions'
 import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
 
@@ -17,7 +18,22 @@ router.on('/').render('pages/home').as('home')
 router
   .get('/movies/:slug', async (ctx) => {
     const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
-    const movie = await fs.readFile(url, 'utf8')
-    return ctx.view.render('pages/movies/show', { movie })
+
+    let movie: any
+
+    try {
+      movie = await fs.readFile(url, 'utf8')
+      ctx.view.share({ movie })
+    } catch (error) {
+      throw new Exception(`Could not find a movie called ${ctx.params.slug}`, {
+        code: 'E_NOT_FOUND',
+
+        status: 404,
+      })
+    }
+
+    return ctx.view.render('pages/movies/show')
   })
   .as('movies.show')
+
+  .where('slug', router.matchers.slug())
